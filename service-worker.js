97272d3ae86a3831c0b7d1a4f9e3bf924cc30134
Masayuki <20tabi.m293@gmail.com>
2019-07-27 22:05:57 +0900

    offline対応のためswを更新

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,34 @@
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-});
+const STATIC_DATA = [
+    './index.html',
+    './css/bootstrap.min.css',
+    './css/bootstrap.min.css.map',
+    './images/icon128.png',
+    './images/icon144.png',
+    './js/bootstrap.bundle.min.js',
+    './js/bootstrap.bundle.min.js.map',
+    './js/jquery-3.4.1.slim.min.js',
+    './app.js'
+  ];
   
+self.addEventListener('install', function(e) {
+e.waitUntil(
+    caches.open('cache_v1').then(function(cache) {
+    return cache.addAll(STATIC_DATA);
+    })
+);
+});
+
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+
+event.respondWith(
+    caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+    })
+);
+});
+  
