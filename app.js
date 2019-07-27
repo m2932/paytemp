300e7a445bf420081ec50c81807d2f250e1c49bd
Masayuki <20tabi.m293@gmail.com>
2019-07-27 21:36:25 +0900

    削除機能の追加

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let payList;
+let appearPayed = true;
 
 const init = () => {
     viewPayList();
@@ -40,12 +41,15 @@ const viewPayList = () => {
     loadAllPayItem();
     let liList = [ ];
     for(const id in payList) {
+        if(!appearPayed && payList[id]["isPayed"] == "checked"){ continue; }
+        
         const itemStr = `
         <li class="list-group-item">
             <div class="form-row align-items-center ispayed-${payList[id]["isPayed"]}" id="${id}_line">
                 <div class="col-1 form-check form-check-inline"><input type="checkbox" class="form-control form-check-input" id="${id}_isPayed" ${payList[id]["isPayed"]} onchange="switchIsPayed(${id});"></div>
-                <div class="col-7"><input type="text" class="form-control" id="${id}_item" value="${payList[id]["item"]}" oninput="edit(${id}, 'item')";></div>
+                <div class="col-6"><input type="text" class="form-control" id="${id}_item" value="${payList[id]["item"]}" oninput="edit(${id}, 'item')";></div>
                 <div class="col-3"><input type="number" class="form-control" id="${id}_value" value="${payList[id]["value"]}" oninput="edit(${id}, 'value')";></div>
+                <div class="col-1"><button class="btn btn-secondary rounded-circle p-0" style="width:2rem;height:2rem;" onclick="deleteItem(${id}); init();">-</button></div>
             </div>
         </li>
         `;
@@ -65,7 +69,6 @@ const switchIsPayed = (id) => {
         document.getElementById(`${id}_line`).classList.remove("ispayed-checked");
     }
     savePayItem(id, payList[id]);
-    // viewPayList();
     sumPaytempValue();
 };
 
@@ -93,4 +96,23 @@ const checkAllIsPayed = () => {
     }
     viewPayList();
     sumPaytempValue();
+};
+
+const deleteItem = (id) => {
+    delete payList[id];
+    localStorage.removeItem(id);
+};
+
+const deleteAllPayed = () => {
+    for (const id in payList) {
+        if(payList[id]["isPayed"] == "checked") {
+            deleteItem(id);
+        }
+    }
+    init();
+};
+
+const switchAppearPayed = () => {
+    appearPayed = !appearPayed;
+    init();
 };
